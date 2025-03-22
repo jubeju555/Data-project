e10ddcf70da8f0291e7aa30a8f91975c9fe74a7e
judah benjamin <judahjosh@gmail.com>
2025-03-22 16:37:15 -0400

    outputting something plus terminal might be ok

--- wranglecode.py
+++ wranglecode.py
@@ -1,6 +1,13 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
-import os
+# import os
 
-file_path = "path/to/marketing_data.csv"
+data_path = "marketing_data.csv"
 marketingdata = pd.read_csv(data_path)
+marketingdata.columns = marketingdata.columns.str.strip()
+
+# convert income to numeric
+marketingdata['Income'] = pd.to_numeric(marketingdata['Income'], errors='coerce')
+
+print(marketingdata['Income'].head())
+
