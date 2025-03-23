e5c1748e2e54004b064838c3ab64a778337755e5
judah benjamin <judahjosh@gmail.com>
2025-03-22 21:35:22 -0400

    little update

--- wranglecode.py
+++ wranglecode.py
@@ -3,12 +3,21 @@ import pandas as pd
 # import os
 
 data_path = "marketing_data.csv"
-marketingdata = pd.read_csv(data_path)
-marketingdata.columns = marketingdata.columns.str.strip()
+datafeed = pd.read_csv(data_path)
+datafeed.columns = datafeed.columns.str.strip()
 
 # convert income to numeric
-marketingdata['ID'] = pd.to_numeric(marketingdata['ID'], errors='coerce')
+datafeed['ID'] = pd.to_numeric(datafeed['ID'], errors='coerce')
+datafeed['Income'] = pd.to_numeric(datafeed['Income'], errors='coerce')
+# group income by educationa and marital status and fill by mean
+datafeed['Income'] = datafeed.groupby(['Education', 'Marital_Status'])['Income'].transform(lambda x: x.fillna(x.mean()))
+# get rid of empty spaces
+datafeed['Education'] = datafeed['Education'].str.strip()
+datafeed['Marital_Status'] = datafeed['Marital_Status'].str.strip()
 
-print(marketingdata['ID'].head())
-print(marketingdata.columns)
-
+# print section to check 
+print(datafeed.head())
+# print(datafeed.columns)
+print(datafeed.shape)
+print(datafeed.info())
+print(datafeed.describe())
