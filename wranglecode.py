4f7612ffd9dc5db7f93b0c00c56bd350b0eed902
judah benjamin <judahjosh@gmail.com>
2025-03-22 22:29:28 -0400

    good income for now, calcs diff values as well

--- wranglecode.py
+++ wranglecode.py
@@ -6,12 +6,16 @@ data_path = "marketing_data.csv"
 datafeed = pd.read_csv(data_path)
 datafeed.columns = datafeed.columns.str.strip()
 
-# convert income to numeric
 datafeed['ID'] = pd.to_numeric(datafeed['ID'], errors='coerce')
+
+# convert income to numeric
+datafeed['Income'] = datafeed['Income'].apply(lambda x: str(x).replace('$', '').replace(',', '').strip())
 datafeed['Income'] = pd.to_numeric(datafeed['Income'], errors='coerce')
-# group income by educationa and marital status and fill by mean
-datafeed['Income'] = datafeed.groupby(['Education', 'Marital_Status'])['Income'].transform(lambda x: x.fillna(x.mean()))
+# group income by education and marital status and fill by mean
+# datafeed['Income'] = datafeed.groupby(['Education', 'Marital_Status'])['Income'].transform(lambda x: x.fillna(x.mean()))
+
 # get rid of empty spaces
+
 datafeed['Education'] = datafeed['Education'].str.strip()
 datafeed['Marital_Status'] = datafeed['Marital_Status'].str.strip()
 
@@ -19,5 +23,8 @@ datafeed['Marital_Status'] = datafeed['Marital_Status'].str.strip()
 print(datafeed.head())
 # print(datafeed.columns)
 print(datafeed.shape)
+# print(datafeed.isnull().sum())
 print(datafeed.info())
 print(datafeed.describe())
+
+
